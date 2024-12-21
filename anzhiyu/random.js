be13e884f915c52784c001b33f3d455a99f4cec8
新大陆苍蓝星 <1434012910@qq.com>
2024-12-21 23:34:19 +0800

    Site updated: 2024-12-21 23:34:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/20/hello-world/","2024/12/21/hexo博客搭建过程/","2024/12/05/images/"];function toRandomPost(){
+var posts=["2024/12/21/hexo博客搭建过程/","2024/12/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
